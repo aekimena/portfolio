6f7391134685cda113504a8779f0527f19996421
Kimena <realarinzemmanuel@gmail.com>
2023-05-31 11:23:13 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById('contact-btn').onclick = () => {
+    document.getElementById('contact-box').style = "display: block;";
+
+    document.getElementById('cancel-btn').onclick = () => {
+        document.getElementById('contact-box').style = "display: none;";
+    }
+}
+
