3ea7b374a20d540321e577038cb9b466c58f4ca2
Arinze Emmanuel <arinzemmanuel23@gmail.com>
2025-11-01 01:22:27 +0100

    fix: button issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -138,11 +138,12 @@ export default function Home() {
                   border
                   border-white
                   bg-transparent
-                  text-white rounded-full px-4 py-2 text-base hover:bg-white/10
+                  text-white rounded-full px-4 py-2 text-sm hover:bg-white/10
                   active:bg-white/20
                   focus:outline-none
                   focus:ring-2
-                  focus:ring-white/30"
+                  focus:ring-white/30 disabled:opacity-50
+disabled:cursor-not-allowed"
                 >
                   View Code
                 </a>
@@ -154,11 +155,12 @@ export default function Home() {
                   border
                   border-white
                   bg-transparent
-                  text-white rounded-full px-4 py-2 text-base hover:bg-white/10
+                  text-white rounded-full px-4 py-2 text-sm hover:bg-white/10
                   active:bg-white/20
                   focus:outline-none
                   focus:ring-2
-                  focus:ring-white/30"
+                  focus:ring-white/30 disabled:opacity-50
+disabled:cursor-not-allowed"
                 >
                   Download APK
                 </a>
@@ -201,7 +203,7 @@ export default function Home() {
 border
 border-white
 bg-transparent
-text-white rounded-full px-4 py-2 text-base hover:bg-white/10
+text-white rounded-full px-4 py-2 text-sm hover:bg-white/10
 active:bg-white/20
 focus:outline-none
 focus:ring-2
